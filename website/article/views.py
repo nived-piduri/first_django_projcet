cae59bc0fe923302748135da3c0a6b90999247a5
krishivpiduri2010 <krishivpiduri337@gmail.com>
2021-07-02 10:26:20 -0700

    Initial commit

---
+++ website/article/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import *
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html', context={'articles': Article.objects.all()})
